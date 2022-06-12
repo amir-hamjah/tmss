854f80fc89d8639cba16bc13123fd898ba6526ba
amir-hamjah <amirhamjah.laam@gmail.com>
2022-06-12 23:33:45 +0600

    Add files via upload

---
+++ js/slick setting.js
@@ -0,0 +1,16 @@
+$('.hero_slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: false,
+  });
+
+  $('.clients_slider').slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToSScroll: 1,
+  })
